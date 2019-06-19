dc1e64aec3a2fdd6b16f388994ab27678934b1b1
prathyusha <vadlamannati.p@husky.neu.edu>
2019-06-19 14:03:22 -0400

    Comments added by P

--- src/empiricalanalysis/OriginalStrassen.java
+++ src/empiricalanalysis/OriginalStrassen.java
@@ -1,20 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package empiricalanalysis;
 
 /**
  *
- * @author shivani bhalchandra
+ * @author prathyusha
  */
 public class OriginalStrassen {
     
-    public int[][] SendMatrixForStrassen(int[][] A, int[][] B, int orignalLength){
-		int D[][] = new int[orignalLength][orignalLength];
+    public int[][] SendMatrixForStrassen(int[][] A, int[][] B, int givenLength){
+		int D[][] = new int[givenLength][givenLength];
 		
-		int C[][] = StrassenMultiply(A, B);
+		int C[][] = strassenMultiplication(A, B);
 		for(int i = 0; i < D.length; i++){
 			for(int j = 0; j < D.length; j++){
 				D[i][j] = C[i][j];
@@ -25,42 +21,42 @@ public class OriginalStrassen {
 	}
     
     
-    private static int[][] StrassenMultiply(int[][] A, int[][] B) {
+    private static int[][] strassenMultiplication(int[][] A, int[][] B) {
 
-        int n = A.length;
+        int halflen = A.length;
 
-        int[][] res = new int[n][n];
+        int[][] result = new int[halflen][halflen];
 
         // if the input matrix is 1x1
-        if (n == 1) {
-            res[0][0] = A[0][0] * B[0][0];
+        if (halflen == 1) {
+            result[0][0] = A[0][0] * B[0][0];
         } else {
 
-            // first matrix
-            int[][] a = new int[n / 2][n / 2];
-            int[][] b = new int[n / 2][n / 2];
-            int[][] c = new int[n / 2][n / 2];
-            int[][] d = new int[n / 2][n / 2];
+        // Dividing the length of first matrix into half recursively
+            int[][] a = new int[halflen / 2][halflen / 2];
+            int[][] b = new int[halflen / 2][halflen / 2];
+            int[][] c = new int[halflen / 2][halflen / 2];
+            int[][] d = new int[halflen / 2][halflen / 2];
             
-            // second matrix
-            int[][] e = new int[n / 2][n / 2];
-            int[][] f = new int[n / 2][n / 2];
-            int[][] g = new int[n / 2][n / 2];
-            int[][] h = new int[n / 2][n / 2];
+         // Dividing the length of second matrix into half recursively 
+            int[][] e = new int[halflen / 2][halflen / 2];
+            int[][] f = new int[halflen / 2][halflen / 2];
+            int[][] g = new int[halflen / 2][halflen / 2];
+            int[][] h = new int[halflen / 2][halflen / 2];
 
-            // dividing matrix A into 4 parts
-            divideArray(A, a, 0, 0);
-            divideArray(A, b, 0, n / 2);
-            divideArray(A, c, n / 2, 0);
-            divideArray(A, d, n / 2, n / 2);
+            // Dividing matrix A into 4 parts
+            arrayDivision(A, a, 0, 0);
+            arrayDivision(A, b, 0, halflen / 2);
+            arrayDivision(A, c, halflen / 2, 0);
+            arrayDivision(A, d, halflen / 2, halflen / 2);
 
-            // dividing matrix B into 4 parts
-            divideArray(B, e, 0, 0);
-            divideArray(B, f, 0, n / 2);
-            divideArray(B, g, n / 2, 0);
-            divideArray(B, h, n / 2, n / 2);
+            // Dividing matrix B into 4 parts
+            arrayDivision(B, e, 0, 0);
+            arrayDivision(B, f, 0, halflen / 2);
+            arrayDivision(B, g, halflen / 2, 0);
+            arrayDivision(B, h, halflen / 2, halflen / 2);
             
-            /** 
+            /** Equations to be formed:
               p1 = (a + d)(e + h)
               p2 = (c + d)e
               p3 = a(f - h)
@@ -70,68 +66,68 @@ public class OriginalStrassen {
               p7 = (b - d) (g + h)
             **/
            
-            int[][] p1 = StrassenMultiply(addMatrices(a, d), addMatrices(e, h));
-            int[][] p2 = StrassenMultiply(addMatrices(c,d),e);
-            int[][] p3 = StrassenMultiply(a, subMatrices(f, h));           
-            int[][] p4 = StrassenMultiply(d, subMatrices(g, e));
-            int[][] p5 = StrassenMultiply(addMatrices(a,b), h);
-            int[][] p6 = StrassenMultiply(subMatrices(c, a), addMatrices(e, f));
-            int[][] p7 = StrassenMultiply(subMatrices(b, d), addMatrices(g, h));
+            int[][] p1 = strassenMultiplication(matricesAddition(a, d), matricesAddition(e, h));
+            int[][] p2 = strassenMultiplication(matricesAddition(c,d),e);
+            int[][] p3 = strassenMultiplication(a, subMatrices(f, h));           
+            int[][] p4 = strassenMultiplication(d, subMatrices(g, e));
+            int[][] p5 = strassenMultiplication(matricesAddition(a,b), h);
+            int[][] p6 = strassenMultiplication(subMatrices(c, a), matricesAddition(e, f));
+            int[][] p7 = strassenMultiplication(subMatrices(b, d), matricesAddition(g, h));
 
             
-           /**
+           /** Operations to perform to get resultant matrix
               C11 = p1 + p4 - p5 + p7
               C12 = p3 + p5
               C21 = p2 + p4
               C22 = p1 - p2 + p3 + p6
             **/
            
-            int[][] C11 = addMatrices(subMatrices(addMatrices(p1, p4), p5), p7);
-            int[][] C12 = addMatrices(p3, p5);
-            int[][] C21 = addMatrices(p2, p4);
-            int[][] C22 = addMatrices(subMatrices(addMatrices(p1, p3), p2), p6);
+            int[][] C11 = matricesAddition(subMatrices(matricesAddition(p1, p4), p5), p7);
+            int[][] C12 = matricesAddition(p3, p5);
+            int[][] C21 = matricesAddition(p2, p4);
+            int[][] C22 = matricesAddition(subMatrices(matricesAddition(p1, p3), p2), p6);
 
             // adding all subarray back into one
-            copySubArray(C11, res, 0, 0);
-            copySubArray(C12, res, 0, n / 2);
-            copySubArray(C21, res, n / 2, 0);
-            copySubArray(C22, res, n / 2, n / 2);
+            copySubArray(C11, result, 0, 0);
+            copySubArray(C12, result, 0, halflen / 2);
+            copySubArray(C21, result, halflen / 2, 0);
+            copySubArray(C22, result, halflen / 2, halflen / 2);
         }
-        return res;
+        return result;
     }
 
 
    
     
-    // helper methods
+    // Helper methods
     
     // Adding 2 matrices
-    public static int[][] addMatrices(int[][] a, int[][] b) {
+    public static int[][] matricesAddition(int[][] a, int[][] b) {
         int n = a.length;
-        int[][] res = new int[n][n];
+        int[][] result = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                res[i][j] = a[i][j] + b[i][j];
+                result[i][j] = a[i][j] + b[i][j];
             }
         }
-        return res;
+        return result;
     }
 
     // Subtracting 2 matrices
     public static int[][] subMatrices(int[][] a, int[][] b) {
-        int n = a.length;
-        int[][] res = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                res[i][j] = a[i][j] - b[i][j];
+        int len = a.length;
+        int[][] result = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                result[i][j] = a[i][j] - b[i][j];
             }
         }
-        return res;
+        return result;
     }
 
   
-    // divides array
-    public static void divideArray(int[][] P, int[][] C, int iB, int jB) 
+    // Divides the array
+    public static void arrayDivision(int[][] P, int[][] C, int iB, int jB) 
     {
         for(int i1 = 0, i2 = iB; i1 < C.length; i1++, i2++)
             for(int j1 = 0, j2 = jB; j1 < C.length; j1++, j2++)
